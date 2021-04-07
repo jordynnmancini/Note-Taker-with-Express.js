a4d22cd90f181ff21e0e7554fcafc488b24b502b
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-07 18:04:03 -0400

    having troubles getting api routes to work - not showing or updating notes from db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,31 @@
 // Load Data 
-const savedNotes = require('../db/db.json');
+const fs = require('fs'); 
+// const savedNotes = require('../db/db.json');
 
 //Routing 
 module.exports = (app) => {
-    app.get('api/notes', (req, res) => res.json(savedNotes));
+    fs.readFile('db/db.json', (err, data) => {
+        if (err) throw err;
+        const savedNotes = JSON.parse(data); 
+
+    app.get('api/notes', (req, res) => res.JSON(savedNotes));
 
     app.post('api/notes', (req, res) => {
-        savedNotes.push(req.body); 
+        savedNotes.push(req.body);
+        updateDB(); 
+        res.send(savedNotes); 
     }); 
 
-    app.delete(`api/notes/${id}`, (req, res) => {
+//     app.delete(`api/notes/${id}`, (req, res) => {
+        
+//     }); 
 
-    }); 
-    
+    function updateDB() {
+        fs.writeFile('db/db.json', JSON.stringify(savedNotes, '/t'), err => {
+            if (err) throw err;
+            return true; 
+        });
+    }
+
+    });
 }; 

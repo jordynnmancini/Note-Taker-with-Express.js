d29a8f4ba83e3173fca57ac1312933e6c38ee5db
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-12 20:45:07 -0400

    updates to make it compatible with heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });  
 }

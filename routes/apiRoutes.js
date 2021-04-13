c40f49c42a150cb79f30660cdb7be05e8da0c313
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-12 20:26:41 -0400

    added uniqid npm package

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 // Load Data 
 const fs = require('fs'); 
 const notes = require('../db/db.json');
+const uniqid = require('uniqid');
+
 
 //Routing 
 module.exports = (app) => {
@@ -8,14 +10,18 @@ module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(notes));
 
     app.post('/api/notes', (req, res) => {
+        req.body.id = uniqid(); 
         notes.push(req.body);
         updateDB(); 
         res.send(notes); 
     }); 
 
-//     app.delete(`api/notes/${id}`, (req, res) => {
-        
-//     }); 
+    // app.delete(`api/notes/:${id}`, (req, res) => {
+    //     fs.readFile('db/db.json', (err,data) => {
+    //         if (err) throw err;
+            
+    //     })
+    // }); 
 
     function updateDB() {
         fs.writeFile('db/db.json', JSON.stringify(notes), err => {
